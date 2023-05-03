f61e81be65a3060a6284fe3b825e2e75eaaff60c
itsrazib1 <itsrazib1@gmail.com>
2023-05-03 20:12:50 +0600

    update more think

--- src/pages/Home/Home/Banner.jsx
+++ src/pages/Home/Home/Banner.jsx
@@ -3,12 +3,12 @@ import { FaSearchengin, FaJenkins, FaBreadSlice, FaHandPeace } from 'react-icons
 
 const Banner = () => {
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2 gap-3 p-5 md:p-10 justify-items-center items-center	'>
+    <div className='border-2 border-black mt-5 rounded-2xl grid grid-cols-1 md:grid-cols-2 gap-3 p-5 md:p-10 justify-items-center items-center	'>
       {/* left side */}
-      <div className='px-2 md:px-5'>
-        <p className=''>------ <span className='text-red-500 tracking-widest'>Chefs + Recipes</span></p>
+      <div className='px-2 md:px-5 border-2 border-sky-400 p-3 rounded-2xl'>
+        
         <h2 className='text-1xl md:text-3xl'>
-          <span className='text-3xl md:text-5xl text-red-800'>Welcome to</span>
+          <span className='text-3xl md:text-5xl text-sky-800 font-bold'>Welcome to</span>
           <span className='underline decoration-base-200 text-3xl md:text-5xl pl-1 md:pl-2 font-bold'>The Spice Garden</span>
         </h2>
         <br />
@@ -18,7 +18,7 @@ const Banner = () => {
           On your website, you can showcase recipes of American cuisines and cultures, including appetizers, entrees, desserts, and more. You can also provide detailed cooking instructions, ingredient lists, and tips and tricks for preparing each dish.
         </p>
         <br />
-        <div className='text-1xl text-red-800 grid grid-cols-2'>
+        <div className='text-1xl text-green-600 grid grid-cols-2'>
           <span className='flex gap-2 justify-items-center items-center'>
             <FaSearchengin />
             Founding year: 2023
@@ -47,25 +47,25 @@ const Banner = () => {
             <img className='max-w-full max-h-full' src="https://img.freepik.com/free-photo/top-view-crawfish-plate_23-2150346565.jpg?size=626&ext=jpg&ga=GA1.1.1047442247.1677490654&semt=ais" />
           </div>
           <div className="carousel-item max-w-screen-lg mx-auto flex justify-center">
-            <img className='max-w-full max-h-full' src="https://i.ibb.co/NTQvPGJ/New-York-Strip-Steak-with-Red-Wine-Sauce.jpg" />
+            <img className='max-w-full max-h-full' src="https://img.freepik.com/free-vector/hand-drawn-indian-cuisine-illustration_23-2149323580.jpg?size=626&ext=jpg&ga=GA1.1.1047442247.1677490654&semt=ais" />
           </div>
           <div className="carousel-item max-w-screen-lg mx-auto flex justify-center">
-            <img className='max-w-full max-h-full' src="https://i.ibb.co/BVXdvGJ/Pesto-Pasta-Salad.jpg" />
+            <img className='max-w-full max-h-full' src="https://img.freepik.com/free-photo/side-lamb-ragout-with-fried-onion-carrot-tomato-sauce-greens-vegetable-salad-table_141793-4744.jpg?size=626&ext=jpg&ga=GA1.1.1047442247.1677490654&semt=ais" />
           </div>
           <div className="carousel-item max-w-screen-lg mx-auto flex justify-center">
-            <img className='max-w-full max-h-full' src="https://i.ibb.co/fQB39V1/Barbecue-Pork-Ribs.jpg" />
+            <img className='max-w-full max-h-full' src="https://img.freepik.com/free-photo/high-angle-chicken-meal_23-2148825122.jpg?size=626&ext=jpg&ga=GA1.1.1047442247.1677490654&semt=ais" />
           </div>
           <div className="carousel-item max-w-screen-lg mx-auto flex justify-center">
-            <img className='max-w-full max-h-full' src="https://i.ibb.co/RD9GBDf/Shrimp-Scampi.jpg" />
+            <img className='max-w-full max-h-full' src="https://img.freepik.com/free-photo/plate-food-with-different-dishes-including-chicken-rice-other-food_1340-24267.jpg?size=626&ext=jpg&ga=GA1.1.1047442247.1677490654&semt=ais" />
           </div>
           <div className="carousel-item max-w-screen-lg mx-auto flex justify-center">
-            <img className='max-w-full max-h-full' src="https://i.ibb.co/j4d93Cf/Macaroni-and-Cheese.jpg" />
+            <img className='max-w-full max-h-full' src="https://img.freepik.com/free-photo/top-view-delicious-fish-meal-tray_23-2148734692.jpg?size=626&ext=jpg&ga=GA1.1.1047442247.1677490654&semt=ais" />
           </div>
           <div className="carousel-item max-w-screen-lg mx-auto flex justify-center">
             <img className='max-w-full max-h-full' src="https://i.ibb.co/Rhv98Hm/Roast-Turkey.jpg" />
           </div>
           <div className="carousel-item max-w-screen-lg mx-auto flex justify-center">
-            <img className='max-w-full max-h-full' src="https://i.ibb.co/YNNZqYF/Mac-and-Cheese.jpg" />
+            <img className='max-w-full max-h-full' src="https://img.freepik.com/free-photo/top-view-delicious-fish-meal-tray_23-2148734692.jpg?size=626&ext=jpg&ga=GA1.1.1047442247.1677490654&semt=ais" />
           </div>
         </div>
       </div>
